4c3838dce9e4e13c66df2bdd83439a91031ab523
barkotenicholas <barkotenicholas@gmail.com>
2022-06-10 13:48:16 +0300

    Added view binding

--- app/src/main/java/com/barkote/mynotes/MainActivity.kt
+++ app/src/main/java/com/barkote/mynotes/MainActivity.kt
@@ -2,10 +2,22 @@ package com.barkote.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.barkote.mynotes.data.CourseInfo
+import com.barkote.mynotes.data.DataManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val dm = DataManager()
+        val adapterCourses =  ArrayAdapter<CourseInfo>(this,android.R.layout.simple_spinner_item,dm.courses.values.toList())
+
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+
     }
 }
