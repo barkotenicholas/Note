597e8d33a3771b6d8bc5f26ecf3f2529dd27b581
barkotenicholas <barkotenicholas@gmail.com>
2022-06-10 15:01:41 +0300

    Added note list activity

---
+++ app/src/main/java/com/barkote/mynotes/NoteList.kt
@@ -0,0 +1,11 @@
+package com.barkote.mynotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+    }
+}
