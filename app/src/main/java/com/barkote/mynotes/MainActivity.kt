5270cd973d490c38d0df2e85d5ff92725e226870
barkotenicholas <barkotenicholas@gmail.com>
2022-06-13 15:04:00 +0300

    Added onPause method

--- app/src/main/java/com/barkote/mynotes/MainActivity.kt
+++ app/src/main/java/com/barkote/mynotes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.barkote.mynotes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.barkote.mynotes.const.EXTRA_NOTE_POSITION
 import com.barkote.mynotes.const.POSITION_NOT_SET
+import com.barkote.mynotes.data.CourseInfo
 import com.barkote.mynotes.data.DataManager
 import com.barkote.mynotes.databinding.ActivityMainBinding
 
@@ -38,6 +40,13 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        binding.spin.setOnItemClickListener { adapterView, view, i, l ->
+            val note = DataManager.notes[notePosition]
+            val course = adapterView.getItemAtPosition(i)
+            note.course = course as CourseInfo
+        }
+
+
     }
 
     private fun displayNote() {
@@ -46,7 +55,10 @@ class MainActivity : AppCompatActivity() {
         binding.title.setText(note.title)
         binding.content.setText(note.text)
 
+        val item =  DataManager.courses.values.toList().indexOf(note.course)
+
         binding.spin.setText(note.course.toString())
+        binding.spin.setSelection(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -65,7 +77,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun moveNext() {
+
         ++notePosition
         displayNote()
+        invalidateOptionsMenu()
+
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+
+        if(notePosition >= DataManager.notes.lastIndex){
+            val menuItem = menu?.findItem(R.id.action_next)
+            if(menuItem != null){
+                menuItem.icon =getDrawable(R.drawable.ic_block)
+                menuItem.isEnabled = false
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(notePosition != POSITION_NOT_SET) {
+
+            saveNote()
+        }
+    }
+
+    private fun saveNote() {
+
+        val note = DataManager.notes[notePosition]
+
+        note.title = binding.title.text.toString()
+        note.text  = binding.content.text.toString()
+
+
+
     }
 }
